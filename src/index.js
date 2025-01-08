0c1201d2b8b224a454836526c0efdf1694c1b339
aydinmfatih <aydinmfatih@gmail.com>
2025-01-08 23:18:30 +0300

    Add react-icons dependency and update Button component to include icons

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import './index.css';
+import "./index.css";
 import App from "./App.js";
 import Button from "./button.js";
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-   <Button primary >Click Me</Button>
-   <Button warning rounded >Click Me</Button>
-   <Button danger rounded >Click Me</Button>
+    <Button className="mb-5" primary>
+      <GoBell />
+      Click Me
+    </Button>
+    <Button warning rounded>
+      <GoDatabase />
+      Click Me
+    </Button>
+    <Button danger rounded>
+      <GoCloudDownload />
+      Click Me
+    </Button>
   </StrictMode>
 );
