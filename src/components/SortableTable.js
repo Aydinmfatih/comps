2c9ce100a7a4ecd84c1f1dfe03abfab828d57f4c
aydinmfatih <aydinmfatih@gmail.com>
2025-01-22 22:10:48 +0300

    Add SortableTable component; refactor TablePage

---
+++ src/components/SortableTable.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Table from "./Table";
+
+function SortableTable(props) {
+  const { config } = props;
+
+  const handleClick = (column) => {
+    console.log(" Sorting by ", column.label);
+  };
+  const updatedConfig = config.map((column) => {
+    if (!column.sortValue) {
+      return column;
+    }
+    return {
+      ...column,
+      header: () => (
+        <th onClick={() => handleClick(column)}>
+          {column.label} IS SORTABLE
+        </th>
+      ),
+    };
+  });
+
+  return <Table {...props} config={updatedConfig} />;
+}
+
+export default SortableTable;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,13 +1,20 @@
 import React from "react";
-
-function Table({ data, config,keyFn }) {
+import { Fragment } from "react";
+function Table({ data, config, keyFn }) {
   const renderedHeaders = config.map((column) => {
+    if (column.header)
+      return <Fragment key={column.label}>{column.header()}</Fragment>;
+
     return <th key={column.label}>{column.label}</th>;
   });
 
   const renderedRows = data.map((item, index) => {
     const renderedCells = config.map((column) => {
-      return <td key={column.label} className="p-3">{column.render(item)}</td>;
+      return (
+        <td key={column.label} className="p-3">
+          {column.render(item)}
+        </td>
+      );
     });
 
     return (
@@ -21,9 +28,7 @@ function Table({ data, config,keyFn }) {
       <thead>
         <tr className="border-b-2">{renderedHeaders}</tr>
       </thead>
-      <tbody>
-        {renderedRows}
-      </tbody>
+      <tbody>{renderedRows}</tbody>
     </table>
   );
 }
